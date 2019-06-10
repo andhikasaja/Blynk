7c8c873cc76b7fc4a5c4598853f74310e5c25b22
muhamad andhika prasetya <44262110+andhikasaja@users.noreply.github.com>
2019-06-11 06:22:35 +0700

    Update Blynk_LED.ino

--- Blynk_LED/Blynk_LED.ino
+++ Blynk_LED/Blynk_LED.ino
@@ -3,20 +3,19 @@
 #include <BlynkSimpleEsp8266.h>
  
 //cek email dan copy paste kan disini
-char auth[] = "token";
+char auth[] = "8d7f25327d664974b78cfb30a3c21aa7";
 
 //isikan nama wifi dan passwordnya
-char ssid[] = "ssid";
-char pass[] = "password";
+char ssid[] = "WINNO";
+char pass[] = "opoyoenake";
  
 void setup()
 {
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass,"iot.radnet.id",8080);
 }
  
 void loop()
 {
   Blynk.run();
 }
- 
